013fe0d0677de773e09b07f79f2d6726f33ba06f
YoannaPo <yoannna.pol@gmail.com>
2022-02-17 23:18:37 +0000

    Add exercise 3

---
+++ changeRed.js
@@ -0,0 +1,16 @@
+
+// 3. Change Red
+
+// Utwórz plik "changeRed.js", który zostanie uwzględniony w dołączonym do misji pliku "change_red.html".
+
+// Kiedy kursor znajduje się nad kwadratem, zmienia on kolor na czerwony.
+
+// Także w tym przypadku nie należy modyfikować pliku HTML.
+
+const squareRed = document.getElementById("square"); //querySelector("#square");
+console.log(squareRed);
+
+// squareRed.addEventListener("mouseenter", () => console.log("Mouse enter"));
+// squareRed.addEventListener("mouseleave", () => console.log("Mouse leave"));
+squareRed.addEventListener("mouseenter", () => squareRed.style.backgroundColor = "red");
+squareRed.addEventListener("mouseleave", () => alert("Mouse leave"));
